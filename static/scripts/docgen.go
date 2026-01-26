6bb5b5492ac397007bcddf8e837a3d6a9362cb2c
Nadine2016 <nadine.spies@solo.io>
2026-01-26 15:24:28 -0500

    GH workflow
    Signed-off-by: Nadine2016 <nadine.spies@solo.io>

---
+++ static/scripts/docgen.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/agentregistry-dev/agentregistry/cmd/arctl/cmd" // Adjust this to point to your root cmd
+	"github.com/spf13/cobra/doc"
+)
+
+func main() {
+	outDir := "./docs/cli"
+	if err := os.MkdirAll(outDir, 0755); err != nil {
+		log.Fatal(err)
+	}
+
+	// Custom front-matter prepender
+	filePrepender := func(filename string) string {
+		name := filepath.Base(filename)
+		base := strings.TrimSuffix(name, filepath.Ext(name))
+		// Replaces underscores with spaces for the title
+		title := strings.ReplaceAll(base, "_", " ")
+		
+		return fmt.Sprintf("---\ntitle: %s\nweight: 10\n---\n\n", title)
+	}
+
+	// Link handler to keep links clean in markdown
+	linkHandler := func(name string) string {
+		return name
+	}
+
+	root := cmd.Root() // Get your root command
+	err := doc.GenMarkdownTreeCustom(root, outDir, filePrepender, linkHandler)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Docs generated in", outDir)
+}
